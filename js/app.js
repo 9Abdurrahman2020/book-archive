83dcec773dd7e10b7d3bfca4b38cbe5078ba0ecd
9abdurrahman2020 <9abdurrahman2020@gmail.com>
2021-09-02 20:32:14 +0600

    minor updates

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const inputField = document.getElementById('input-field');
 const inputValue = inputField.value;
 textToDynamicLink(inputValue);
 inputField.value = '';
+document.getElementById('book-container').innerHTML = '';
+document.getElementById('result-found').innerHTML = '';
 })
 //function to make link dynamic based on what user search  and simple error handle
 const textToDynamicLink = searchText =>{
@@ -13,8 +15,6 @@ const textToDynamicLink = searchText =>{
         // start spinner 
         document.getElementById('spinner').classList.remove('d-none');
         document.getElementById('error').innerText = '';
-        document.getElementById('book-container').innerHTML = '';
-        document.getElementById('result-found').innerHTML = '';
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetchData(url);
     }
@@ -32,7 +32,7 @@ const dataLoader = data =>{
     // no result error 
     if(data.numFound === 0){
        const error = document.getElementById('error');
-       error.innerHTML = `No book found`
+       error.innerHTML = `No book found`;
     }else{
             // show result quantity
     const result = document.getElementById('result-found');
@@ -44,14 +44,14 @@ const dataLoader = data =>{
     array.forEach(obj =>{
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="col">
-    <div class="card">
-      <img src=" https://covers.openlibrary.org/b/id/${obj.cover_i? obj.cover_i: '10909258'}-M.jpg" class="card-img-top" alt="...">
+    <div class="col h-100">
+    <div class="card p-3 h-100">
+      <img class="" src=" https://covers.openlibrary.org/b/id/${obj.cover_i? obj.cover_i: '10909258'}-M.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Book Name: ${obj.title}</h5>
-        <p class=""><b>Author Name:</b> ${obj.author_name}</p>
-        <p class=""><b>Publisher Name:</b> ${obj.publisher?.[0]}</p>
-        <p class=""><b>First Publish:</b> ${obj.publish_date?.[0]}</p>
+        <h5 class="text-center">${obj.title}</h5>
+        <p class=""><b>Author:</b> ${obj.author_name}</p>
+        <p class=""><b>Publisher:</b> ${obj.publisher?.[0]}</p>
+        <p class=""><b>Publish:</b> ${obj.publish_date?.[0]}</p>
       </div>
     </div>
     </div>
